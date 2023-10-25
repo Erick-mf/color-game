89a2fa09bd224f759a2ee7f606d037d819d3fc8c
Erick-mf <ericknmejia@gmail.com>
2023-10-25 10:11:09 +0200

    Niveles y colores de los div terminado

--- script.js
+++ script.js
@@ -3,6 +3,24 @@ window.onload = () => {
 
     btn_1.addEventListener("click", () => {
         iniciarJuego();
+        btn_2.disabled = false;
+        btn_3.disabled = false;
+    })
+
+    let btn_2 = document.getElementById("btn-2");
+
+    btn_2.addEventListener("click", () => {
+        nivelFacil();
+        btn_2.disabled = true;
+        btn_3.disabled = true;
+    })
+
+    let btn_3 = document.getElementById("btn-3");
+
+    btn_3.addEventListener("click", () => {
+        nivelDificil();
+        btn_3.disabled = true;
+        btn_2.disabled = true;
     })
 }
 
@@ -14,7 +32,31 @@ function randomColor() {
     return `RGB(${red}, ${green}, ${blue})`;
 }
 
+function generarColores(num) {
+    let seccionColores = document.querySelector('.colores');
+
+    for (let i = 0; i < num; i++) {
+        let color = randomColor();
+        seccionColores.innerHTML += `<div style="background-color: ${color};"></div>`;
+    }
+}
+
+function nivelFacil() {
+    generarColores(3);
+}
+
+function nivelDificil() {
+    generarColores(6);
+}
+
 function iniciarJuego() {
+    let limpiar_colores = document.querySelector('.colores');
+
+    limpiar_colores.innerHTML = '';
+
     document.getElementById("rgb").innerHTML = randomColor();
+
+    let fondo_titulo = document.getElementById("titulo");
+    fondo_titulo.style.backgroundColor = randomColor();
 }
 
